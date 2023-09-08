a9a21952bdc9c8467b8f7b9626be922d5428c70d
Ondřej Čertík <ondrej@certik.us>
2023-09-08 16:59:30 -0600

    Finish the benchmark

--- ref.py
+++ ref.py
@@ -1,11 +1,22 @@
 from timeit import default_timer as clock
 import numpy as np
 
-n = 1024*4*2
+n = 1024*4
 
 A = np.empty((n,n), dtype=np.float32)
 B = np.empty((n,n), dtype=np.float32)
 t1 = clock()
 C = np.dot(A, B)
 t2 = clock()
-print(f"Time: {t2-t1}s")
+
+GHz = 1e9
+t = t2-t1
+fma_clock = 0.0625
+freq = 3.2*GHz
+measured = t * freq / n**3
+percent_peak = fma_clock / measured * 100
+print(f"Time: {t}s")
+print("Clock cycles per element:")
+print(f"Theoretical performance peak: {fma_clock} cycles")
+print(f"Measured:                     {measured} cycles")
+print(f"Percent peak:                 {percent_peak}%")
