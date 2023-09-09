1335f125de24ac4c5c90c8474c14ad9042a152b7
Ondřej Čertík <ondrej@certik.us>
2023-09-08 18:31:11 -0600

    Add matmul2

--- cpu.cpp
+++ cpu.cpp
@@ -8,6 +8,19 @@ void matmul1(const float *a, const float *b, float *c, int n) {
                 c[i * n + j] += a[i * n + k] * b[k * n + j];
 }
 
+void matmul2(const float *a, const float *_b, float *c, int n) {
+    float *b = new float[n * n];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            b[i * n + j] = _b[j * n + i];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < n; k++)
+                c[i * n + j] += a[i * n + k] * b[j * n + k]; // <- note the indices
+}
+
 int main() {
     //int n = 1920;
     int n = 1024;
@@ -15,7 +28,7 @@ int main() {
     float *b = (float*) malloc(n*n*sizeof(float));
     float *c = (float*) malloc(n*n*sizeof(float));
     auto t1 = std::chrono::high_resolution_clock::now();
-    matmul1(a, b, c, n);
+    matmul2(a, b, c, n);
     auto t2 = std::chrono::high_resolution_clock::now();
     double t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.0;
     double GHz = 1e9;
